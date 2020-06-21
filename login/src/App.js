36ab67901d350d0803fe00f369d1f2743d77a04f
Raphaelli Bellini <rafael.bellini@hotmail.com>
2020-06-21 19:43:37 -0300

    [ADD] Criar função de login

--- login/src/App.js
+++ login/src/App.js
@@ -1,9 +1,33 @@
 import React, { Component} from 'react';
 import './App.css';
 import { Grid, Header, Form, Segment, Button } from 'semantic-ui-react';
+import If from './If';
+import api from './utils/api';
 
 class LoginForm extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      username:'',
+      password:'', 
+      resp: {}
+    }
+  }
+
+  submit = async() => {
+    await api.post('/usuario/login', {login: this.state.username, senha: this.state.password}).then((resp) => {
+      this.setState({resp});
+      if(this.state.resp.data !== 'Usuário ou senha inválidos!') {
+        this.setState({ redirect: true })
+      }
+    }).catch((error) => {
+      console.log('error', error);
+    });  
+  }
+
   render() {
+    const { resp } = this.state
+
     return (
       <Grid textAlign='center' className='grid' verticalAlign='middle'>
         <Grid.Column className='column'>
@@ -12,18 +36,22 @@ class LoginForm extends Component {
           </Header>
           <Form size='large'>
             <Segment stacked>
-              <Form.Input fluid icon='user' iconPosition='left' placeholder='E-mail' />
+              <Form.Input fluid icon='user' iconPosition='left' placeholder='E-mail' onChange={(evt) => this.setState({ username: evt.target.value})}/>
               <Form.Input
                 fluid
                 icon='lock'
                 iconPosition='left'
                 placeholder='Senha'
                 type='password'
+                onChange={(evt) => this.setState({ password: evt.target.value})}
               />
 
-              <Button className='btn' fluid size='large' >
+              <Button className='btn' fluid size='large' onClick={() => this.submit()}>
                 Login
               </Button>
+              <If condition={resp.data === 'Usuário ou senha inválidos!'}>
+                  <span className='loginmsg'>Login/Senha inválidos!</span>
+              </If>
             </Segment>
           </Form>
         </Grid.Column>
